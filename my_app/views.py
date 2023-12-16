89e27002beccff51cbc0206aa9093b3525f7a40e
vladyatsuk <yatsuk.vladislav@gmail.com>
2023-12-16 02:49:16 +0200

    [Add] category endpoints

--- my_app/views.py
+++ my_app/views.py
@@ -51,3 +51,30 @@ def user_add():
 @app.get('/users')
 def users_get():
     return list(users.values())
+
+@app.get('/category')
+def categories_get():
+    return list(categories.values())
+
+@app.post('/category')
+def category_add():
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        name = request.json['name']
+    else:
+        name = request.args.get('name')
+    id = uuid.uuid4().hex
+    category = {'id': id, 'name': name}
+    categories[id] = category
+    return category
+
+@app.delete('/category')
+def category_delete():
+    category_id = request.args.get('category_id')
+    if not category_id:
+        return {'error', 'Category id is required'}, 400
+    if category_id not in categories:
+        return {'error': f'No category found with id {category_id}'}, 404
+    else:
+        del categories[category_id]
+        return {'message': f'Category {category_id} deleted successfully'}
